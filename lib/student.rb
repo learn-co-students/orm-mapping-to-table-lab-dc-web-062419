faad168d2515921c5fb0ba634cb7af98c8aeed80
github username <github email address>
2019-07-07 14:37:36 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -30,12 +30,18 @@ DB[:conn].execute(sql)
 end
 
 def save
-sql = <<-SQL
-INSERT into students (name, grade)
-VALUES(?, ?)
-SQL
+    if self.id
+        self.update
+    else
+    sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+    SQL
+
 DB[:conn].execute(sql, self.name, self.grade)
+
 @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students" )[0][0]
+    end
 end
 
 def self.create(name:, grade:)
